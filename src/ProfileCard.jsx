95d5c8db24f91f2fda146a8de7a8df6d018d2371
arittaw <arittamova@gmail.com>
2025-03-27 19:06:29 -0700

    cambion en ProfileCard.jsx con algunas propiedades

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,9 +1,23 @@
 function ProfileCard ({title, handle, image}){
-  console.log(title)
-  console.log(handle)
-  console.log(image)
+  
 return (
-    <div>ProfileCard</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="" width={200} height={200} />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">title is {title}</p>
+          <p className="subtitle is-5"></p>
+        </div>
+      </div>
+
+    </div>
+
+    
   )
 }
 
